95705a3ae26564778f23bec1678811b93e9b4773
Abhijit Agarwal <abhijit.148@gmail.com>
2014-04-02 03:20:03 +0530

    Movement.py for a new algo that supports concurrent moves

--- arrows.py
+++ arrows.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from datetime import datetime
 
 #'Database' of images
 arrows={
@@ -8,6 +9,45 @@ arrows={
 	'se':'images/se.png',
 	'sw':'images/sw.png'
 }
+
+
+#Draws a list of moves on an image by calling other functions
+def drawMoves(img,moves,start,limit):
+	for move in moves:
+		img=putMove(img,start,limit,move)
+	return img
+
+#Main purpose of this method is to figure out the position where arrow should be superimposed
+#and then call the superimposer - overlayImage
+def putMove(img,start,limit,move):
+	current=datetime.now()
+	dir,timing=move
+
+	centery = np.size(img, 1)/2
+	centerx = np.size(img, 0)/2
+
+	offsetx=centerx*(current-start).microseconds/(limit*100000)
+	offsety=centery*(current-start).microseconds/(limit*100000)
+
+	posx,posy=getcoords(centerx,centery,offsetx,offsety,dir)
+
+	fore=cv2.imread(arrows[dir])
+
+	if posx>=2*centerx-np.size(fore, 0):
+		posx=2*centerx-np.size(fore, 0)-1
+
+	if posy>=2*centery-np.size(fore, 1):
+		posy=2*centery-np.size(fore, 1)-1
+
+	if posx<0:
+		posx=0
+
+	if posy<0:
+		posy=0
+
+	#print offsetx,offsety
+	img=overlayImage(img,fore,posx,posy)
+	return img
 
 #This function superimposes an image fore (without black) on another image back at coordinate x and y
 def overlayImage(back,fore,x,y):
@@ -24,25 +64,14 @@ def overlayImage(back,fore,x,y):
 	return output
 
 #This function gets the next coordinate in a particular direction
-def getcoords(x,y,dir):
+def getcoords(x,y,x1,y1,dir):
 	if dir=='se':
-		return x+10,y+10
+		return x+x1,y+y1
 	elif dir=='ne':
-		return x-10,y+10
+		return x-x1,y+y1
 	elif dir=='sw':
-		return x+10,y-10
+		return x+x1,y-y1
 	elif dir=='nw':
-		return x-10,y-10
+		return x-x1,y-y1
 	else:
 		return x,y
-
-#A function to move an arrow on a bacground image - back
-def animate(back,x,y,dir):
-	fore=cv2.imread(arrows[dir])
-	x,y=getcoords(x,y,dir)
-	if x>=np.size(back, 0)-np.size(fore, 0) or y>=np.size(back, 1)-np.size(fore, 1) or x<0 or y<0:
-			x=np.size(back, 0)/2
-			y=np.size(back, 1)/2
-	#print x,y,np.size(back, 1)-np.size(fore, 1),np.size(back, 0)-np.size(fore, 0)
-	output=overlayImage(back,fore,x,y)
-	return output,x,y
--- main.py
+++ main.py
@@ -1,74 +1,52 @@
 import cv2
 import numpy as np
-import arrows
 from datetime import datetime
 
-def diffImg(t0, t1, t2):
-  d1 = cv2.absdiff(t2, t1)
-  d2 = cv2.absdiff(t1, t0)
-  return cv2.bitwise_and(d1, d2)
+#implemented separately in same folder
+import arrows
+import movement
 
-cam = cv2.VideoCapture(0)
-
+#Create a window
 imgWindow="Image from Camera"
 cv2.namedWindow(imgWindow)
 
 # Read three images first:
+cam = cv2.VideoCapture(0)
 t_minus = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
 t = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
 img=cam.read()[1]
 t_plus = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-
-#Calculation of width and height using numpy
-width = np.size(img, 1)
-height = np.size(img, 0)
-
-#The moves data
-moves=[("nw",1.5),
-  ("ne",3.5),
-  ("sw",6),
-  ("se",8.5) ]
-
-#The coords
-coords={
-  "nw":(0,50,0,50),
-  "ne":(0,50,width-50,width),
-  "sw":(height-50,height,0,50),
-  "se":(height-50,height,width-50,width) 
-}
-
-#Other initialization variables
-x,y=height/2,width/2
-start=datetime.now()
-i=0
-
-score=0
-
 #Initial flipping for mirror effect
 t_minus = cv2.flip(t_minus,1)
 t = cv2.flip(t,1)
 t_plus = cv2.flip(t_plus,1)
 
+#Calculation of width and height using numpy
+width = np.size(img, 1)
+height = np.size(img, 0)
+
+#Other initialization variables
+start=datetime.now()
+score=0
+limit=2
+
+#The moves data
+moves=[("nw",1.5),
+  ("ne",1.5),
+  ("sw",3.5),
+  ("se",4.5) ]
+
+
 while True:
   #Printing out the scores
   cv2.putText(img, "Score: "+str(score),(width/3,height/9) , cv2.FONT_HERSHEY_PLAIN, 2.0, color=(0,0,0), thickness=2)
 
-  #For each move
-  if i<len(moves):
-    dir,timing=moves[i] #timing is the time under which the action must be performed.
-    y1,y2,x1,x2=coords[dir]
-    diff=diffImg(t_minus[y1:y2, x1:x2], t[y1:y2, x1:x2], t_plus[y1:y2, x1:x2]) 
-    current=datetime.now()
-    if cv2.countNonZero(diff) < 1750 and (current-start).total_seconds()<=timing: #1750 is our threshold
-      text_color = (255,0,0) #color as (B,G,R)
-      img,x,y=arrows.animate(img,x,y,dir)
-
-    else:
-      if(current-start).total_seconds()<=timing:
-        score+=25
-
-      i+=1
-      x,y=height/2,width/2
+  completed,notCompleted=movement.completed(moves,start,t_minus,t,t_plus,limit)
+  img=arrows.drawMoves(img,notCompleted,start,limit)
+  score+=25*len(completed)
+  for move in moves:
+    if move in completed:
+      moves.remove(move)
 
   cv2.imshow(imgWindow,img)
 
@@ -79,7 +57,6 @@ while True:
 
   #Continuous flipping for mirror effect
   img = cv2.flip(img,1)
-  
   t_plus = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
   key = cv2.waitKey(10)
